2fcf2b9edacaace6cb5a872cfcb3ed15a3fe642a
tuananh <tutuananh975@gmail.com>
2023-04-19 01:44:37 +0700

    routing

---
+++ client/src/pages/home.js
@@ -0,0 +1,9 @@
+import MainLayout from "../../layout/MainLayout";
+
+export default function home() {
+    return (
+        <MainLayout>
+            <div>Home page</div>
+        </MainLayout>
+    )
+}

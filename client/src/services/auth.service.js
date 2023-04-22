9c82fe9c36f3856b1f48474a56ef1c8a93345c14
NgCan <118377905+Decan209@users.noreply.github.com>
2023-04-22 13:54:34 +0700

    pull

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,10 +1,15 @@
 import axios from "axios";
 
-export const registerService = async ({dataAcc}) => {
-  const res = await axios.post("http://localhost:5000/api/v1/auth/register",dataAcc)
-    // headers: {
-    //   "content-type": "application/json",
-    // },
-//   });
-  console.log(res);
+export const registerService = async (dataAcc) => {
+  const res = await fetch("http://localhost:5000/api/v1/auth/login", {
+    method:"POST",
+    body: JSON.stringify({
+      dataAcc
+    }),
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+//   console.log("dataACc",JSON.stringify(dataAcc));
+console.log(res);
 };

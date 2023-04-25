17e02098becc38c307812fe7001a6871df147f27
NgCan <118377905+Decan209@users.noreply.github.com>
2023-04-26 00:08:17 +0700

    update homepage render data

--- client/src/components/ProductList/ProductList.js
+++ client/src/components/ProductList/ProductList.js
@@ -5,14 +5,14 @@ const ProductList = ({ data }) => {
   return (
     <>
       <div className="grid grid-cols-4 gap-4 max-md:grid-cols-3 max-sm:grid-cols-2">
-        {data.map((data) => (
-          <div className="p-4" key={data.id}>
-            <img src={data.img} alt="" className="rounded-lg" />
-            <div className="text-xl font-medium font-serif text-center mt-3">
+        {data?.map((data) => (
+          <div className="p-4" key={data._id}>
+            <img src={data.avatar} alt="" className="rounded-lg h-64 w-full" />
+            <div className="text-xl font-medium font-mono text-center mt-3 h-20">
               {data.name}
             </div>
             <div className="text-center font-medium text-gray-300 font-serif">
-              {data.nameEng}
+              {data.englishName}
             </div>
           </div>
         )
--- client/src/pages/home/HomePage.js
+++ client/src/pages/home/HomePage.js
@@ -4,37 +4,69 @@ import Poster from "./components/Poster/Poster";
 import SlickSlide from "./components/SlickSlide/SlickSlide";
 import SideBarList from "./components/SideBarList/SideBarList";
 import ProductList from "@/components/ProductList/ProductList";
-
+import { ComingDb } from "@/data/ComingSoon";
+import { useQueries } from "react-query";
+import {
+  hotMoviesService,
+  newSingleMoiveService,
+  newSeriesMoiveService,
+} from "@/services/Homepage.service";
+import { BeatLoader } from "react-spinners";
 
 export default function HomePage() {
+  const queries = [
+    { queryKey: "filmHot", queryFn: hotMoviesService },
+    { queryKey: "newSingleFilm", queryFn: newSingleMoiveService },
+    { queryKey: "newSeriesFilm", queryFn: newSeriesMoiveService },
+  ];
+  const results = useQueries(queries);
+
+  if (results.some((result) => result.isLoading)) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <BeatLoader color="hsla(33, 100%, 50%, 1)" margin={3} size={20} />
+      </div>
+    );
+  }
+
+  if (results.some((result) => result.isError)) {
+    return <div>Error!</div>;
+  }
+
+  const filmHot = results[0].data;
+  const newSingleFilm = results[1].data;
+  const newSeriesFilm = results[2].data;
+
   return (
     <MainLayout>
       <div>
-        <Poster/>
-        <div className="text-2xl font-semibold pl-8 mt-8 ml-9 font-serif">
+        <Poster />
+        <div className="text-2xl font-semibold pl-8 mt-8 ml-9 font-momo">
           Phim Hot
         </div>
-        <SlickSlide data={FaturedMovies} />
+        <SlickSlide data={filmHot} />
         <div className="flex max-sm:block max-md:block max-lg:block">
-          <div className="w-3/4 p-4 max-sm:w-full max-md:w-full max-lg:w-full">
-            <div className="text-2xl font-semibold pl-4 mt-8 ml-4 font-serif">
-              Phim Mới Cập Nhật
+          <div className="w-3/4 p-4 max-sm:w-full max-md:w-full max-lg:w-full mt-2">
+            <div className="text-2xl font-semibold pl-4 mt-8 ml-4 font-mono mb-2">
+              Phim Lẻ Mới Cập Nhật
             </div>
-            <ProductList data={FaturedMovies} />
-            <div className="text-2xl font-semibold pl-4 mt-8 ml-4 font-serif">
-              Phim Hoạt Hình
+            <ProductList data={newSingleFilm} />
+            <div className="text-2xl font-semibold pl-4 mt-8 ml-4 font-mono">
+              Phim Bộ Mới Cập Nhật
             </div>
-            <ProductList data={FaturedMovies} />
+            <ProductList data={newSeriesFilm} />
           </div>
           <div>
-            <div className="max-sm:hidden max-md:hidden max-lg:hidden">
-              <div className="text-2xl font-semibold p-4 mt-8 ">
+            <div className="max-sm:hidden max-md:hidden max-lg:hidden p-2 w-11/12">
+              <div className="text-2xl font-semibold p-4 mt-8 font-mono ">
                 Phim Sắp Ra Mắt
               </div>
-              <SideBarList data={FaturedMovies} />
+              <SideBarList data={ComingDb} />
             </div>
             <div className="max-sm:px-2 max-md:px-3 max-lg:p-3">
-              <div className="text-2xl font-semibold p-4 mt-8">TRENNDING</div>
+              <div className="text-2xl font-semibold p-4 mt-8 font-mono">
+                TRENNDING
+              </div>
               <SideBarList data={FaturedMovies} />
             </div>
           </div>
